a6d6d722ea107ed364e3ed4fdcd50b6ad4baed5f
geosok <sokolovgeorgy@gmail.com>
2021-10-21 21:16:49 +0300

    Выполнено в класе 21.10.2021.

--- Занятие3/Лабораторные_задания/task1_4/main.py
+++ Занятие3/Лабораторные_задания/task1_4/main.py
@@ -1,7 +1,9 @@
-def sum_func(epsilon):
-    epsilon = 0.0001
-    while
-
+def sum_func(epsilon = 0.0001):
+    n = 1
+    sum_ = 1/n
+    while sum_ > epsilon:
+        sum_ += 1
+        n += 1
 if __name__ == "__main__":
     # Write your solution here
     print(sum_func())
