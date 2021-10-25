84cb766c930bb25cb6620543a7ef8ca1bbae8f0d
gyoscha <sokolovgeorgy@gmail.com>
2021-10-25 14:10:20 +0300

    дз 3 (25.10.2021)

--- Занятие3/Домашнее_задание/task3/main.py
+++ Занятие3/Домашнее_задание/task3/main.py
@@ -1,3 +1,15 @@
+def func_(str_):
+    list_words = str_.split()
+    print(list_words)
+    max_words = []
+    max_len = max(len(value) for value in list_words)
+    for value in list_words:
+        if len(value) == max_len:
+            max_words.append(value)
+    return max_words
+
+
 if __name__ == "__main__":
     # Write your solution here
-    pass
+    str_words = 'В случае, когда самых длинных""" слов может быть несколько, вернуть их списком'
+    print(func_(str_words))
